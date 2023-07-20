30b23a1d91e3f09857017b46e14164811574d8cc
Dev-anshika98 <g.anshikacreative@gmail.com>
2023-07-20 15:05:44 +0530

    created skeleton & Home page

---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+
+import App from "./App";
+import { Home } from "./pages";
+import "./index.css";
+import { ContextProvider } from "./context/Context";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    //   {
+    //     path: "/about",
+    //     element: <About />,
+    //   },
+    //   {
+    //     path: "/service",
+    //     element: <Service />,
+    //   },
+    //   {
+    //     path: "/portfolio",
+    //     element: <Portfolio />,
+    //   },
+    //   {
+    //     path: "/contact",
+    //     element: <Contact />,
+    //   },
+    // 
+  ],
+  },
+]);
+
+root.render(
+  <ContextProvider>
+    <RouterProvider router={router} />
+  </ContextProvider>
+);
